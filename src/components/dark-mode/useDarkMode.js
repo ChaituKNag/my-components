94d75facd5481a93683e3b30b3c9452af493ef1a
Naga Chaitanya Konada <knc.dev@outlook.com>
2020-12-12 22:49:45 +0530

    component and file cleanup

--- src/components/dark-mode/useDarkMode.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { DarkModeContext } from './DarkModeProvider'
-const useDarkMode = () => {
-    const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
-
-    return [darkMode, toggleDarkMode];
-}
-
-export default useDarkMode;
